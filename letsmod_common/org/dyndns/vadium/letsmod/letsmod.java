8a9bf7f898ebd27ea931435266969c5472ebf625
Magnus Ågren <pc@tripnet.se>
2013-04-20 09:25:08 +0200

    Initial commit

---
+++ letsmod_common/org/dyndns/vadium/letsmod/letsmod.java
@@ -0,0 +1,30 @@
+package org.dyndns.vadium.letsmod;
+
+import org.dyndns.vadium.letsmod.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class letsmod {
+
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) { 
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event) {
+        
+    }
+}
---
+++ letsmod_common/org/dyndns/vadium/letsmod/lib/Reference.java
@@ -0,0 +1,14 @@
+package org.dyndns.vadium.letsmod.lib;
+
+public class Reference {
+
+    /* Debug Mode on-Off */
+    public static final boolean DEBUG_MODE = false;
+    
+    /* General MOD related constants */
+    public static final String MOD_ID = "LetsMod";
+    public static final String MOD_NAME = "Let's Mod";
+    public static final String VERSION = "0.0.1";
+           
+    
+}
